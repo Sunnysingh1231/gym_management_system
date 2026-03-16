f85638e983e35e36788ec632d26ed0ab3e45384c
Sunnysingh1231 <123saktisingh@gmail.com>
2026-03-16 23:09:06 +0530

    project created

---
+++ construct_your_body/src/main/java/com/cyb/ConstructYourBodyApplication.java
@@ -0,0 +1,13 @@
+package com.cyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructYourBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructYourBodyApplication.class, args);
+	}
+
+}
