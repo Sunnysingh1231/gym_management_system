b24224adb23140cfad1e9568aff6f8bcd5109a18
Sunnysingh1231 <123saktisingh@gmail.com>
2026-03-17 09:32:36 +0530

    entities

---
+++ construct_your_body/src/main/java/entity/ActiveMemberDetails.java
@@ -0,0 +1,19 @@
+package entity;
+
+public class ActiveMemberDetails {
+
+	private int id;
+	
+	private String fullName;
+	
+	private String email;
+	
+	private String gender = "NaN";
+	
+	private String phone = "**********";
+	
+	private String memberShipType;
+	
+	private String joinDate;
+	
+}
---
+++ construct_your_body/src/main/java/entity/MemberShipPaymentDetail.java
@@ -0,0 +1,25 @@
+package entity
+
+
+y
+public class MemberShipPaymentDetail {
+	
+	private int id;
+	
+	private String email;
+	
+	private String memberShipType;
+	
+	private String memberShipPrice;
+	
+	private String memberShipStartDate;// = currentDate.format(formatter);
+
+	private String memberShipEndDate;//= ScurrentDate.format(formatter);
+	
+	private String paymenDateTime;
+	
+	private long attendence;
+	
+	private boolean isAccountExpire;
+	
+}
