80a8eb27f6151c1c4105920148795b64073dfcbd
Sunnysingh1231 <123saktisingh@gmail.com>
2026-03-21 23:09:11 +0530

    user entity and dashboard

---
+++ construct_your_body/src/main/java/com/cyb/entity/User.java
@@ -0,0 +1,28 @@
+package com.cyb.entity;
+
+
+public class User {
+
+	private int id;
+	 
+	private int age;
+	
+	private int height;
+	
+	private float weight;
+	
+	private String fullName;
+	
+	private String email;
+	
+	private String gender;
+	
+	private String phone;
+	
+	private String hasRole;
+	
+	private String password;
+	
+	
+
+}
